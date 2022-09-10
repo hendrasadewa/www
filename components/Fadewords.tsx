d93b0f89851d9044d8c81104185a939d72daac72
hendrasadewa <hendrasadewa30@gmail.com>
2022-09-10 21:42:58 +0700

    feat: hero page

---
+++ components/Fadewords.tsx
@@ -0,0 +1,53 @@
+import { motion, Variants } from 'framer-motion';
+import { useCallback, useEffect, useState } from 'react';
+
+interface Props {
+  words: Array<string>;
+  className?: string,
+}
+
+const variants: Variants = {
+  show: {
+    opacity: 1,
+    transition: {
+      ease: 'easeOut',
+      duration: 2,
+    },
+  },
+  hide: {
+    opacity: 0,
+  },
+};
+
+function FadeWords({ words, className }: Props) {
+  const [wordIndex, setWordIndex] = useState(0);
+
+  const length = words.length;
+  const currentWord = words[wordIndex];
+
+  const handleIntervalChange = useCallback(() => {
+    setWordIndex((prev) => (prev + 1 <= length - 1 ? prev + 1 : 0));
+  }, [length]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      handleIntervalChange();
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, [handleIntervalChange]);
+
+  return (
+    <motion.p
+      key={wordIndex}
+      variants={variants}
+      initial="hide"
+      animate="show"
+      className={className}
+    >
+      {currentWord}
+    </motion.p>
+  );
+}
+
+export default FadeWords;
---
+++ constants/siteInfo.ts
@@ -0,0 +1,10 @@
+export const occupations = [
+  'A passionate web engineer 👨🏻‍💻',
+  'A seasoned writer 📇',
+  'A React.js addict ⚛️',
+  'A gig rat 🙌',
+  'A cat lover 🐈',
+  'An #odapi survivor 🩸',
+  'A tarnished 🗡',
+  'A weird music enjoyer 🎧',
+];
