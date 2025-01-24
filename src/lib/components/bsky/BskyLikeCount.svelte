5232da806e86424bb8c0bfabc6770b5ed56f5555
hendra sadewa <me@hendrasadewa.com>
2025-01-24 09:54:29 +0700

    move the bsky promise to onmounted

--- src/lib/components/bsky/BskyLikeCount.svelte
+++ src/lib/components/bsky/BskyLikeCount.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
 	import { Heart, LoaderIcon } from 'lucide-svelte';
 	import { GetPostLikes as getPostLikes } from '$lib/services/bsky/publicAPI';
+	import { onMount } from 'svelte';
+	import type { LikesResponse } from '$lib/services/bsky/types';
 
 	export let postURI = '';
+
+	let response: LikesResponse = { likes: [], uri: '' };
+	let isLoading = false;
+
+	onMount(async () => {
+		isLoading = true;
+		response = await getPostLikes(postURI);
+		isLoading = false;
+	});
 </script>
 
-{#await getPostLikes(postURI)}
+{#if isLoading}
 	<LoaderIcon class="animate-spin" />
-{:then response}
+{:else}
 	<div class="flex items-center gap-1">
 		<Heart size="12" />
 		<span class="md:text-md text-xs"
 			>{response.likes.length} likes from bsky</span
 		>
 	</div>
-{:catch}
-	<span class="md:text-md text-xs">failed to get likes</span>
-{/await}
+{/if}
